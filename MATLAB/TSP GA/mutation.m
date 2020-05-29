bb76b62606278be6a867b506959cbbea2897ddd1
Oleg Tsirkov <33631875+Shempp@users.noreply.github.com>
2020-05-30 00:24:03 +0300

    Rus symbols

--- MATLAB/TSP GA/mutation.m
+++ MATLAB/TSP GA/mutation.m
@@ -1,17 +1,17 @@
 function NewPopulation=mutation(OldPopulation,Pm)
 [SizeOfPopulation,SizeOfChrom]=size(OldPopulation);
-% ’ромосомы, выбранные дл¤ мутации
+% Хромосомы, выбранные для мутации
 SelChromMut = rand(SizeOfPopulation,1)<=Pm;
 NewPopulation=OldPopulation;
 for i=1:SizeOfPopulation
     if ~(SelChromMut(i)==0)
-        % ¬ыбор случайного гена хромосомы
+        % Выбор случайного гена хромосомы
         ChooseGen=randi(SizeOfChrom);
-        % ѕолучение случайного числа дл¤ мутации
-        % «десь необходимо учитывать, что i-й элемент списка Ц это
+        % Получение случайного числа для мутации
+        % Здесь необходимо учитывать, что i-й элемент списка i это
         % номер от 1 до n-i+1
         RandValue=randi(SizeOfChrom-ChooseGen+1);
-        % ћен¤ем исходную попул¤цию
+        % Меняем исходную популяцию
         NewPopulation(i,ChooseGen)=RandValue;
     end
 end
