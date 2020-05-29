2ab118869732c9e297e9e0b9a8fc3660451b8183
Oleg Tsirkov <33631875+Shempp@users.noreply.github.com>
2020-05-30 00:21:01 +0300

    Rus symbols

--- MATLAB/TSP GA/crossover.m
+++ MATLAB/TSP GA/crossover.m
@@ -3,27 +3,27 @@ if nargin==1
     Pc=0.5;
 end
 
-% –азмер попул¤ции
+% Размер популяции
 SizeOfPop=size(OldPopulation,1); 
-%  оличество пар дл¤ кроссовера
+% Количество пар для кроссовера
 NumOfPares=floor(SizeOfPop/2);   
-% ѕары хромосом, выбранные случайно дл¤ скрещивани¤
+% Пары хромосом, выбранные случайно для скрещивания
 SelPairsCros=rand(NumOfPares,1)<=Pc;  
 NewPopulation=OldPopulation;
 
 for i=1:NumOfPares
     Index=(i-1)*2+1;
     if ~(SelPairsCros(i)==0)
-       % ѕерва¤ хромосома из пары
+       % Первая хромосома из пары
        FirstChrom=OldPopulation(Index,:);
-       % ¬тора¤ хромосома из пары
+       % Вторая хромосома из пары
        SecondChrom=OldPopulation(Index+1,:);
-       % √енераци¤ точки скрещивани¤
-	   CrossPoint=randi(length(FirstChrom));
-       %  лассический кроссовер
+       % Генераци¤ точки скрещивания
+       CrossPoint=randi(length(FirstChrom));
+       % Классический кроссовер
        FirstChild=[FirstChrom(1:CrossPoint) SecondChrom(CrossPoint+1:end)];
        SecondChild=[SecondChrom(1:CrossPoint) FirstChrom(CrossPoint+1:end)];
-       % ћен¤ем исходную попул¤цию
+       % Меняем исходную популяцию
        NewPopulation(Index,:)=FirstChild;
        NewPopulation(Index+1,:)=SecondChild;
     end
