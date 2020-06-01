b955188a981a30c1b5db69b38ee9e569b4726d32
Shempp <oltsirkov@mail.ru>
2020-06-02 01:51:45 +0300

    Fix error in model

--- Python/RentalProperty/rentalproperty/rentalpropertyapp/models.py
+++ Python/RentalProperty/rentalproperty/rentalpropertyapp/models.py
@@ -11,5 +11,3 @@ class RentingItem(models.Model):
     price = models.IntegerField()
     address = models.CharField(max_length=256)
     #property_type = models.CharField(max_length=16, choices=PROPERTY_TYPES)
-    def __str__(self):
-        return self.price + self.address
