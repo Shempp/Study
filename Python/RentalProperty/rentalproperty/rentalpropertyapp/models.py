b88590a1ef57c2fc601b2042bdede56815cac80c
Shempp <oltsirkov@mail.ru>
2020-06-02 01:47:48 +0300

    Added model for 'rentalpropertyapp'

--- Python/RentalProperty/rentalproperty/rentalpropertyapp/models.py
+++ Python/RentalProperty/rentalproperty/rentalpropertyapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+#PROPERTY_TYPES = [
+#    ('TP1', 'Type1'),
+#    ('TP2', 'Type2'),
+#]
+
+class RentingItem(models.Model):
+    price = models.IntegerField()
+    address = models.CharField(max_length=256)
+    #property_type = models.CharField(max_length=16, choices=PROPERTY_TYPES)
+    def __str__(self):
+        return self.price + self.address
