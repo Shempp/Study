0a873336bb11430032304d53a32ebcd4dda5d68c
Shempp <oltsirkov@mail.ru>
2020-06-03 18:33:24 +0300

    Getting data from JSON file

--- Python/Django/RentalProperty_on_REST/rentalproperty/rentalpropertyapp/static/renting-items-list/renting-items-list.component.js
+++ Python/Django/RentalProperty_on_REST/rentalproperty/rentalpropertyapp/static/renting-items-list/renting-items-list.component.js
@@ -6,27 +6,36 @@ angular.
     component('rentingItemsList', {  // This name is what AngularJS uses to match to the `<renting-items-list>` element.
         // Note: The URL is relative to our `home.html` file
         templateUrl: '/static/renting-items-list/renting-items-list.template.html',
-        controller: function RentingItemsListController() {
-            var currentDate = new Date();
-            this.rentingItems = [
-                {
-                    name: 'Saint-Petersburg, st. Begovaya, 10',
-                    price: '25000',
-                    pub_date: currentDate,
-                }, {
-                    name: 'Saint-Petersburg, st. Begovaya, 11',
-                    price: '20000',
-                    pub_date: currentDate,
-                }, {
-                    name: 'Saint-Petersburg, st. Begovaya, 12',
-                    price: '15000',
-                    pub_date: currentDate,
-                } , {
-                    name: 'Anapa, st. Pushkina, 1',
-                    price: '9000',
-                    pub_date: currentDate,
-                }
-            ];
-            this.orderProp = 'price';
-        }
+        controller: ['$http', function RentingItemsListController($http) {
+                var self = this;
+                self.orderProp = 'price';
+                // Make http request to server to fetch the data in json file
+                $http.get('/static/RentingItems.json').then(function(response) {
+                    self.rentingItems = response.data;
+                });
+
+                /*
+                var currentDate = new Date();
+                this.rentingItems = [
+                    {
+                        address: 'Saint-Petersburg, st. Begovaya, 10',
+                        price: '25000',
+                        pub_date: currentDate,
+                    }, {
+                        address: 'Saint-Petersburg, st. Begovaya, 11',
+                        price: '20000',
+                        pub_date: currentDate,
+                    }, {
+                        address: 'Saint-Petersburg, st. Begovaya, 12',
+                        price: '15000',
+                        pub_date: currentDate,
+                    } , {
+                        address: 'Anapa, st. Pushkina, 1',
+                        price: '9000',
+                        pub_date: currentDate,
+                    }
+                ];
+                this.orderProp = 'price';
+                 */
+        }]
     });
